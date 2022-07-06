71a80d94fdd454b100fa2fed0c6063bc88ef1a64
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-07-07 02:19:25 +0600

    new colletion part is done

---
+++ src/Allpages/NewColletion/NewColletion.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NewColletion = () => {
+    return (
+        <div className='mt-[100px]'>
+            <div className=' text-center mb-[55px]'>
+                <p className='text-[#a0a0a0]'>NEW COLLECTION</p>
+                <h1 className='text-[40px] font-semibold mt-[12px] mb-[27px]'>Best Picks 2021</h1>
+                <p className='text-[#a0a0a0]'>A conscious collection made entirely from food crop waste, recycled <br />cotton, other more sustainable materials. </p>
+            </div>
+        </div>
+    );
+};
+
+export default NewColletion;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import Banner from './Allpages/Banner/Banner';
 import DownBanner from './Allpages/DownBanner/DownBanner';
 import Nav from './Allpages/Navbar/Nav';
+import NewColletion from './Allpages/NewColletion/NewColletion';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='font-sans'>
       {/* navbar part */}
       <Nav />
       {/* Banner part  */}
       <Banner />
       {/* DownBanner part */}
       <DownBanner />
+      {/* NEW COLLECTION part */}
+      <NewColletion />
     </div>
   );
 }
